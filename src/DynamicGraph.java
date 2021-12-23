7a40dab7c33e1951aacc1e30dc6edacad173d74c
shalom2552 <shalom2552@gmail.com>
2021-12-24 00:38:06 +0200

    Added LinkedList

--- src/DynamicGraph.java
+++ src/DynamicGraph.java
@@ -2,43 +2,57 @@ public class DynamicGraph {
 
     RootedTree tree;
 
+    LinkedList<GraphNode> nodes;
+    LinkedList<GraphEdge> edges;
 
     // default constructor
     public DynamicGraph(){
+        this.nodes = null;
+        this.edges = null;
     }
-    
+
 
     public GraphNode insertNode(int nodeKey){
-        GraphNode node = new GraphNode(nodeKey, null, null);
-        GraphNode[] old_nodes = this.tree.nodes;
-        int num_nodes = old_nodes.length;
-        GraphNode[] new_nodes = new GraphNode[num_nodes + 1];
-        for (int i = 0; i < num_nodes; i++) {
-            new_nodes[i] = old_nodes[i];
-        }
-        new_nodes[num_nodes + 1] = node;
-        this.tree.nodes = new_nodes;
-        return node;
+        Node<GraphNode> node = new Node<>(new GraphNode(nodeKey, null, null), null, null);
+        this.nodes.Insert(node);
+        return node.pointer;
     }
 
     public void deleteNode(GraphNode node){
-        // TODO those algorithms can't be implemented that way!!
-        // TODO they have to be O(1), then maybe linked lists?
-        if (node.getInDegree() == 0 && node.getOutDegree() == 0){
-            int key = node.getKey();
-            GraphNode[] old_nodes = this.tree.nodes;
-            int num_nodes = old_nodes.length;
-            GraphNode[] new_nodes = new GraphNode[num_nodes - 1];
-            int index = 0;
-            for (GraphNode old_node : old_nodes) {
-                if (old_node.getKey() != key) {
-                    new_nodes[index] = old_node;
-                    index++;
-                }
-            }
-            this.tree.nodes = new_nodes;
-        }
+
     }
+
+//    public GraphNode insertNode(int nodeKey){
+//        GraphNode node = new GraphNode(nodeKey, null, null);
+//        GraphNode[] old_nodes = this.tree.nodes;
+//        int num_nodes = old_nodes.length;
+//        GraphNode[] new_nodes = new GraphNode[num_nodes + 1];
+//        for (int i = 0; i < num_nodes; i++) {
+//            new_nodes[i] = old_nodes[i];
+//        }
+//        new_nodes[num_nodes + 1] = node;
+//        this.tree.nodes = new_nodes;
+//        return node;
+//    }
+//
+//    public void deleteNode(GraphNode node){
+//        // TODO those algorithms can't be implemented that way!!
+//        // TODO they have to be O(1), then maybe linked lists?
+//        if (node.getInDegree() == 0 && node.getOutDegree() == 0){
+//            int key = node.getKey();
+//            GraphNode[] old_nodes = this.tree.nodes;
+//            int num_nodes = old_nodes.length;
+//            GraphNode[] new_nodes = new GraphNode[num_nodes - 1];
+//            int index = 0;
+//            for (GraphNode old_node : old_nodes) {
+//                if (old_node.getKey() != key) {
+//                    new_nodes[index] = old_node;
+//                    index++;
+//                }
+//            }
+//            this.tree.nodes = new_nodes;
+//        }
+//    }
 
     public GraphEdge insertEdge(GraphNode from, GraphNode to){
         return null;
